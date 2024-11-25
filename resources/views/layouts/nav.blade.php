d03685db5b7dbfc28ca04a80e2d5af883327af92
FrancisCastillo24 <castillofernandezfrancis@gmail.com>
2024-11-25 22:46:27 +0100

    SASS  y Bootstrap personalizado
    He configurado estilos SASS para header y nav para Bootstrap.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">LOGO</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
+    <div class="collapse navbar-collapse justify-content-around text-lg-start text-center" id="navbarNav">
+      <ul class="navbar-nav gap-3 gap-lg-5"> <!-- Ajustamos los espacios -->
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
@@ -16,7 +16,10 @@
           <a class="nav-link" href="#">Pricing</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Disabled</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Prueba</a>
         </li>
       </ul>
     </div>
