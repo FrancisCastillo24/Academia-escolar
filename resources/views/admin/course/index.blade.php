415d7e9bcf07bc4d3151be673bfd372b731106c2
FrancisCastillo24 <162831161+FrancisCastillo24@users.noreply.github.com>
2024-12-17 00:16:45 +0100

    Panel administrador
    He configurado el SASS para el modelo curso y con ello desarrollando el CRUD de dicho modelo

--- resources/views/admin/course/index.blade.php
+++ resources/views/admin/course/index.blade.php
@@ -2,25 +2,40 @@
 @section('title', 'Cursos')
 
 @section("content")
-<h1>PANEL ADMINISTRATIVO</h1>
-<form action="{{ route('course.store') }}" method="post">
-    @csrf <!-- Token de seguridad obligatorio en Laravel -->
-    <div>
-        <label for="name">Nombre del curso</label>
-        <input type="text" name="name" id="name">
+<div class="content">
+    <div class="form_course">
+        <form action="{{ route('course.store') }}" method="post" class="formulario_curso">
+            @csrf <!-- Token de seguridad en Laravel -->
+
+            <!-- Campo Título -->
+            <div class="mb-4">
+                <label for="name" class="form-label">Título del curso</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+
+            <!-- Campo Descripción -->
+            <div class="mb-4">
+                <label for="course_description" class="form-label">Descripción del curso</label>
+                <input type="text" name="description" id="description" class="form-control">
+            </div>
+
+            <!-- Campo Fecha de inicio -->
+            <div class="mb-4">
+                <label for="start_date" class="form-label">Fecha de inicio</label>
+                <input type="date" name="start_date" id="start_date" class="form-control" required>
+            </div>
+
+            <!-- Campo Fecha de fin -->
+            <div class="mb-4">
+                <label for="start_end" class="form-label">Fecha de fin</label>
+                <input type="date" name="start_end" id="start_end" class="form-control" required>
+            </div>
+
+            <!-- Botón de enviar -->
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary px-4 py-2">Crear Curso</button>
+            </div>
+        </form>
     </div>
-    <div>
-        <label for="description">Descripción del curso</label>
-        <textarea name="description" id="description"></textarea>
-    </div>
-    <div>
-        <label for="start_date">Inicio del curso</label>
-        <input type="date" name="start_date" id="start_date">
-    </div>
-    <div>
-        <label for="start_end">Fin del curso</label>
-        <input type="date" name="start_end" id="start_end">
-    </div>
-    <button type="submit">Crear Curso</button>
-</form>
+</div>
 @endsection
