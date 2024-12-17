29813e8e7a476846d16cb9c096de28c56c24b694
FrancisCastillo24 <162831161+FrancisCastillo24@users.noreply.github.com>
2024-12-17 23:29:45 +0100

    SASS finalizado
    He finalizado con SASS para la vista course index

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -27,3 +27,13 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
+
+<script>
+        // JavaScript para manejar el menú en dispositivos móviles
+        const menuToggle = document.getElementById('menu-toggle');
+        const sidebar = document.getElementById('sidebar');
+
+        menuToggle.addEventListener('click', () => {
+            sidebar.classList.toggle('open');
+        });
+    </script>
--- resources/views/layouts/navAdmin.blade.php
+++ resources/views/layouts/navAdmin.blade.php
@@ -1,15 +1,27 @@
-<div class="sidebar">
-    <h2>Admin Panel</h2>
-    <ul>
-        <li><a href="{{ route('admin.courses.index') }}">Clases</a></li>
-        <li><a href="#">Alumnos</a></li>
-        <li><a href="#">Talleres</a></li>
-        <li><a href="#">Reseñas</a></li>
-        <li>
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="dropdown-item">Panel Usuario</button>
-            </form>
-        </li>
-    </ul>
+<div class="admin">
+    <div class="sidebar">
+        <h2>Admin Panel</h2>
+        <ul>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle" id="dropdownToggle">
+                    Clases
+                    <i class="arrow"></i> <!-- Esta flecha solo está en el menú principal -->
+                </a>
+                <ul class="dropdown-menu" id="dropdownMenu">
+                    <li><a href="{{ route('admin.courses.index') }}">Crear</a></li>
+                    <li><a href="{{ route('admin.courses.index') }}">Modificar</a></li>
+                    <li><a href="{{ route('admin.courses.index') }}">Eliminar</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Alumnos</a></li>
+            <li><a href="#">Talleres</a></li>
+            <li><a href="#">Reseñas</a></li>
+            <li>
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="dropdown-item">Panel Usuario</button>
+                </form>
+            </li>
+        </ul>
+    </div>
 </div>
